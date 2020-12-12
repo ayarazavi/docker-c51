e886a3f4f82a60ca5ea181cbf19a35c6b8bbc195
Ahad <ahad@vemba.com>
2020-12-12 18:15:28 -0500

    added offer model

---
+++ src/src/Entity/Offer.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OfferRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OfferRepository::class)
+ */
+class Offer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=510)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=510)
+     */
+    private $image_url;
+
+    /**
+     * @ORM\Column(type="decimal", precision=15, scale=2)
+     */
+    private $cash_back;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->image_url;
+    }
+
+    public function setImageUrl(string $image_url): self
+    {
+        $this->image_url = $image_url;
+
+        return $this;
+    }
+
+    public function getCashBack(): ?string
+    {
+        return $this->cash_back;
+    }
+
+    public function setCashBack(string $cash_back): self
+    {
+        $this->cash_back = $cash_back;
+
+        return $this;
+    }
+}
